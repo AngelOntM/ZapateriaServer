c82452a880ce852fe90898e791120a83cdd5a7ac
AngelOntM <ajom0507@hotmail.com>
2022-03-15 11:29:08 -0600

    Token 2.0

--- app/Controllers/Http/Users/AuthController.ts
+++ app/Controllers/Http/Users/AuthController.ts
@@ -1,5 +1,5 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import User from 'App/Models/User'
+import User from 'App/Models/Users'
 import { rules, schema } from '@ioc:Adonis/Core/Validator'
 
 export default class AuthController {
@@ -8,7 +8,6 @@ export default class AuthController {
         const validations = await schema.create({
             email: schema.string({}, [rules.email(), rules.unique({ table: 'users', column: 'email' })]),
             password: schema.string({}, [rules.confirmed()]),
-            username: schema.string({}, [rules.unique({ table: 'users', column: 'username' })]),
         })
         const data = await request.validate({ schema: validations })
         const user = await User.create(data)
